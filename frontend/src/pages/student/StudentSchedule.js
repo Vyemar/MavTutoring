e54bfcc1ce0b5b90285a6e06ab888a4a4ac2203f
Jemimah Ogunderu <jmo8657@mavs.uta.edu>
2025-07-25 00:22:49 -0500

    update upcoming sessions endpoint

--- frontend/src/pages/student/StudentSchedule.js
+++ frontend/src/pages/student/StudentSchedule.js
@@ -50,11 +50,12 @@ function StudentSchedule() {
     fetchUserSession();
   }, []);
 
+  //should hit a backend endpoint to fetch just upcoming sessions instead
   const fetchUpcomingSessions = useCallback(async () => {
     if (!userData || !userData.id) return;
 
     try {
-      const response = await axios.get(`${BACKEND_URL}/api/sessions/student/${userData.id}`, {
+      const response = await axios.get(`${BACKEND_URL}/api/sessions/student/upcoming/${userData.id}`, {
         withCredentials: true
       });
       setUpcomingSessions(response.data);
@@ -159,7 +160,7 @@ function StudentSchedule() {
 
       if (response.data.success) {
         setSuccessMessage('Session booked successfully!');
-        const res = await sendNotification(response.data.session._id);
+        await sendNotification(response.data.session._id);
         fetchUpcomingSessions();
         setSelectedDate('');
         setSelectedTutor('');
